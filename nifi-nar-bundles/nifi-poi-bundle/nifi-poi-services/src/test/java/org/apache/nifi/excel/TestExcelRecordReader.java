f388e2f8b977b185978210c095ca1917fb2e321e
nathluu <luuhoangtanbis@gmail.com>
2024-03-14 15:39:36 +0700

    Debug log 3

--- nifi-nar-bundles/nifi-poi-bundle/nifi-poi-services/src/test/java/org/apache/nifi/excel/TestExcelRecordReader.java
+++ nifi-nar-bundles/nifi-poi-bundle/nifi-poi-services/src/test/java/org/apache/nifi/excel/TestExcelRecordReader.java
@@ -60,7 +60,7 @@ public class TestExcelRecordReader {
                 .build();
 
         MalformedRecordException mre = assertThrows(MalformedRecordException.class, () -> new ExcelRecordReader(configuration, getInputStream("notExcel.txt"), logger));
-        fail("Test failed with exception: " + mre.getMessage());
+        fail("Test failed with exception: " + ExceptionUtils.getStackTrace(mre));
         assertTrue(ExceptionUtils.getStackTrace(mre).contains("this is not a valid OOXML"));
     }
 
